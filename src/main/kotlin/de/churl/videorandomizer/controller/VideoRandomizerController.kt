b10a1646f60562160bb3b9ac258b5f32c895fb1e
churl <tobi@urpost.de>
2022-01-26 23:07:10 +0100

    add form submit controller

--- src/main/kotlin/de/churl/videorandomizer/controller/VideoRandomizerController.kt
+++ src/main/kotlin/de/churl/videorandomizer/controller/VideoRandomizerController.kt
@@ -1,9 +1,12 @@
 package de.churl.videorandomizer.controller
 
+import de.churl.videorandomizer.model.Video
 import de.churl.videorandomizer.repository.VideoRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class VideoRandomizerController(
@@ -14,5 +17,11 @@ class VideoRandomizerController(
     fun index(model: Model): String {
         model.addAttribute("videos", videoRepository.findAll())
         return "index";
+    }
+
+    @PostMapping("/add")
+    fun addLink(@RequestParam videourl: String): String {
+        videoRepository.save(Video(videourl))
+        return "redirect:/"
     }
 }
